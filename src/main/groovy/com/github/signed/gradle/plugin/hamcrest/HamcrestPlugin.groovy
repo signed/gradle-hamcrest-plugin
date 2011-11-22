f52bcb14a1bded61c14526d20a7ccf8a61f3795b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2011-11-22 23:16:08 +0100

    Start to collect arguments for the generator call

--- src/main/groovy/com/github/signed/gradle/plugin/hamcrest/HamcrestPlugin.groovy
+++ src/main/groovy/com/github/signed/gradle/plugin/hamcrest/HamcrestPlugin.groovy
@@ -1,12 +1,29 @@
 package com.github.signed.gradle.plugin.hamcrest;
 
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 
 class HamcrestPlugin implements Plugin<Project> {
     def void apply(Project project) {
         project.task('hello') << {
-            println "Hello from the GreetingPlugin"
+            if (project.plugins.hasPlugin('java')) {
+                println 'java plugin applied'
+                def sourcePaths = project.sourceSets*.java.srcDirs.flatten().join(',')
+                println sourcePaths
+                def destinationDir = new File(project.buildDir, 'generated-src')
+                //destinationDir.mkdirs()
+                println destinationDir
+                def configurationFile = new File("file-matchers.xml")
+                println "${configurationFile} exists: ${configurationFile.exists()}"
+            }
+
+            if (project.plugins.hasPlugin('groovy')) {
+                println 'groovy plugin applied'
+//                project.sourceSets.all.groovy.srcDirs*.each createDirs
+            }
+
         }
+        project.tasks.hello.description = 'Collects all factory methods and puts them in a single class'
     }
 }
